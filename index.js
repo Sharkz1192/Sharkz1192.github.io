ca420d7aacf50076ae9009d8628a38ed6d77398d
Sharkz1192 <kjod05@yahoo.com>
2018-09-22 12:46:23 -0400

    first

---
+++ index.js
@@ -0,0 +1,5 @@
+function playsound(){
+    //alert('U smell like dog poo sereously take a shower... Click ok to take a shower!');
+    var pump=document.getElementById("pump") ;
+    pump.play();
+}
