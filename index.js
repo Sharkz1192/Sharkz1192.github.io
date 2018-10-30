9d5c8c58860ecbc5e227011c5c75cb82bbdf9e4f
Sharkz1192 <43500573+Sharkz1192@users.noreply.github.com>
2018-10-30 18:37:56 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
 function playsound(){
-    //alert('U smell like dog poo sereously take a shower... Click ok to take a shower!');
+    //alert('U smell like dog poo seriously take a shower... Click ok to take a shower!');
     var pump=document.getElementById("pump") ;
     pump.play();
+}
+var loc="Updates";
+var link;
+var inter;
+var href;
+function animate(){
+    clearTimeout(inter);
+    window.location=href;
+  
+}
+$(document).ready(function(){
+    $(".Tabs a").click(function(event){
+     href=this.href;
+     event.preventDefault();
+     if (!href.includes(".html#")){
+        var name="unload";
+        link=document.getElementsByClassName("CoverPictures")[0];
+        link.id=name;
+        inter=setTimeout(animate, 200);  
+     }
+    });
+});
+
+function updates() {
+    loc = "Updates";   
+}
+
+function Weapons() {
+    loc = "Weapons";   
+}
+
+function Items() {
+    loc = "Items";
+}
+
+function Skins() {
+    loc = "Skins";   
 }
